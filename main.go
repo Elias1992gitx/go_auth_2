6d9569c10e6b6664957d1614d2199f337f712a9a
Elias Chane <eliashailu492@gmail.com>
2025-02-14 20:41:50 +0300

    go microsrvice

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+        "fmt"
+        "log"
+        "net/http"
+
+        "go-micro.dev/v4/web"
+)
+func main() {
+        // Create a new service
+        service := web.NewService(
+                web.Name("helloworld"),
+                web.Address(":8080"),
+        )
+        // Initialize the service
+        service.Init()
+
+        //set up a route and handler
+        http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request){
+                fmt.Fprintln(w, "Hello, world!")
+        })
+
+        //Assign the handler to the service
+        service.Handle("/", http.DefaultServeMux)
+
+        // Start the service
+        if err := service.Run(); err != nil {
+                log.Fatal(err)
+        }
+}
